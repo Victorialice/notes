59556341bba83d8b82620c7f070ab7595a56a97a
zhuwenqian <1506675573@qq.com>
2017-05-10 12:31:35 +0800

    mysqlconf

--- mysql/mysql_conf.rb
+++ mysql/mysql_conf.rb
@@ -1,5 +1,8 @@
-udo find / -name "*.cnf"
+sudo find / -name "*.cnf"
 
 /usr/local/Cellar/mysql56/5.6.32/my.cnf
 
 sudo /usr/local/Cellar/mysql56/5.6.32/support-files/mysql.server restart
+
+innodb_file_per_table
+innodb_file_format = Barracuda
