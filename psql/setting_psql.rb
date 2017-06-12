2d20942f2ac93b0b91941bfb6788bbdf6d3d4db3
zhuwenqian <1506675573@qq.com>
2017-06-12 16:19:48 +0800

    psql 登录

--- psql/setting_psql.rb
+++ psql/setting_psql.rb
@@ -1,2 +1,3 @@
 启动psql: pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start
 建立用户: /usr/local/Cellar/postgresql/9.5.5/bin/createuser -s postgres
+登录: psql -U postgres -h localhost
