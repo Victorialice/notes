3307d85797844281fabd0007a5f0338a87243b32
zhuwenqian <1506675573@qq.com>
2017-09-01 14:01:18 +0800

    add pg restart

--- psql/setting_psql.rb
+++ psql/setting_psql.rb
@@ -1,3 +1,4 @@
 启动psql: pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start
 建立用户: /usr/local/Cellar/postgresql/9.5.5/bin/createuser -s postgres
 登录: psql -U postgres -h localhost
+重启: brew services restart postgresql
