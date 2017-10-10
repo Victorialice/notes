c135995b1fd925c3a7f049cccc560d7c81be0b9a
zhuwenqian <1506675573@qq.com>
2017-10-10 14:37:29 +0800

    添加psql restart

--- psql/setting_psql.rb
+++ psql/setting_psql.rb
@@ -2,3 +2,8 @@
 建立用户: /usr/local/Cellar/postgresql/9.5.5/bin/createuser -s postgres
 登录: psql -U postgres -h localhost
 重启: brew services restart postgresql
+
+rm -rf /usr/local/var/postgres && initdb /usr/local/var/postgres -E utf8
+
+zhuwenqian.com
+/etc/init.d/postgresql-9.2 start
