02390e6ff3dadfd595df790dc179a7352f82fc8e
zhuwenqian <1506675573@qq.com>
2021-08-15 13:58:58 +0800

    add fashion html

---
+++ ruby_rails/ruby.md
@@ -0,0 +1,18 @@
+# super 调用父类的同名方法/方法查找链中查找它上一层级的同名方法
+# 模块 不能实例化
+# include extend prepend
+# ruby 特点
+  * 解释型
+  * 泛型
+  * OOP
+  * Module
+  * 反射
+  * 垃圾回收 GC
+  * 为程序员而生, 而不是计算机 => 性能问题，团队合作
+  * Everything is an object
+  * Lightweight
+  * cross platform
+# 变量的引用地址 变量内容的存储地址
+# lambda VS proc
+  * proc 参数可以不传
+  * lambda 参数必须有
--- test.rb
+++ test.rb
@@ -1,1 +1,50 @@
-asdfasdfasdfasd
+module ActiveRecord
+	class Base
+		def initialize(attributes = nil)
+			@attributes = attributes_from_column_definition
+		end
+
+		def attribute_names
+			@attributes.keys.sort	
+		end
+
+		alias_method :respond_to_without_attributes?, :respond_to?
+
+		def respond_to?(method)
+			@@dynamic_methods ||= attribute_names + 
+			                      attribute_names.collect { |attr| attr + "=" } +
+			                      attribute_names.collect { |attr| attr + "?" }
+			@@dynamic_methods.include?(method.to_s) ? true : respond_to_without_attributes?(method)
+		end
+
+		def method_missing(method_id, *arguments)
+			method_name = method_id.id2name
+
+			if method_name =~ read_method? && @attributes.include?($1)
+				return read_attrbute($1)
+			elsif method_name =~ write_method?
+				write_attribute($1, arguments[0])
+			elsif method_name =~ query_method?
+				return query_attribute($1)
+			else
+				super
+			end
+		end
+
+		def read_method?() // end
+			
+		end
+	end
+end
+
+module ActiveRecord
+	module AttributeMethods
+		def method_missing(method_id, *args, &block)
+			method_name = method_id.to_s
+
+			if self.class.private_method_defined?(method_name)
+				raise NoMethodError.new("Attempt to call private method", method_name, args)
+			end
+		end
+	end
+end
